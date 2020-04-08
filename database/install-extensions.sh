2968a076d0643e63d7350d2d4494b7f91cce8b57
Matthew Kendon <matt@outlandish.com>
2020-04-08 17:44:44 +0100

    added new dockerfile for creating the database

---
+++ database/install-extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE EXTENSION if not exists pg_trgm;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "${POSTGRES_DB}_test" <<-EOSQL
+    CREATE EXTENSION if not exists pg_trgm;
+EOSQL
